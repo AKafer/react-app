8245f06cc6d032c32a7c2086bd8b72e57c19a000
Sergei Storozhuk <s_storozhuk@wargaming.net>
2025-03-10 16:25:21 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import LeftPanel from './layouts/LefaPanel/LeftPanel.jsx';
+import Body from './layouts/Body/Body.jsx';
+import Header from './components/Header/Header.jsx';
+import JournalList from './components/JournalList/JournalList.jsx';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx';
+import JournalForm from './components/JournalForm/JournalForm.jsx';
+import {useState} from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const INITIAL_DATA = [
+		// {
+		// 	id: 1,
+		// 	title: 'Подготовка в обновлению курсов',
+		// 	date: new Date(),
+		// 	text: 'Сегодня я начал подготовку к обновлению курсов.' +
+		//   ' Я решил начать с курса по React.' +
+		//   ' Первым делом я решил обновить все зависимости.' +
+		//   ' После этого я приступил к обновлению кода.' +
+		//   ' Я начал с компонента Button.'
+		// },
+		// {
+		// 	id: 2,
+		// 	title: 'Обновление компонента Button',
+		// 	date: new Date(),
+		// 	text: 'Сегодня я начал обновление компонента Button.' +
+		//   ' Я решил начать с обновления стилей.' +
+		//   ' После этого я приступил к обновлению кода.' +
+		//   ' Я начал с обновления импортов.'
+		// }
+	];
+
+	const [items, setItems] = useState(INITIAL_DATA);
+
+	const addItem = item => {
+		setItems([...items, {
+			title: item.title,
+			date: new Date(item.date),
+			text: item.text,
+			id: items.length >0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+		}]);
+	};
+
+
+	return (
+		<div className='app'>
+			<LeftPanel>
+				<Header/>
+				<JournalAddButton/>
+				<JournalList items={items}/>
+			</LeftPanel>
+			<Body>
+				<JournalForm onSubmit={addItem}/>
+			</Body>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/JournalForm/JournalForm.jsx
@@ -0,0 +1,26 @@
+import './JournalForm.css';
+import Button from '../Button/Button';
+
+function JournalForm({ onSubmit }) {
+	const addJournalItem = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const formProps = Object.fromEntries(formData);
+		onSubmit(formProps);
+	};
+
+	return (
+		<>
+			<form className='journal-form' onSubmit={addJournalItem}>
+				<input type="text" name='title'/>
+				<input type="date" name='date'/>
+				<input type="text" name='tag' />
+				<textarea name='text' cols='30' rows='10'></textarea>
+				<Button text='Сохранить'/>
+			</form>
+
+		</>
+	);
+}
+
+export default JournalForm;
